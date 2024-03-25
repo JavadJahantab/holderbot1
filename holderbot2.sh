af69f191cc431f30f43bb5931e3df68a1283b235
Javad Jahantab <101731790+JavadJahantab@users.noreply.github.com>
2024-03-25 23:56:37 +0330

    Update holderbot2.sh

--- holderbot2.sh
+++ holderbot2.sh
@@ -205,8 +205,8 @@ while true; do
     fi
 done
 
-crontab -l | grep -vF "/bin/bash /holderbot/restart2.sh" | crontab -
-cronjob="@reboot sleep 20 && /bin/bash /holderbot/restart2.sh"
+crontab -l | grep -vF "/bin/bash ./holderbot2/restart2.sh" | crontab -
+cronjob="@reboot sleep 20 && /bin/bash ./holderbot2/restart2.sh"
 if ! crontab -l | grep -Fq "$cronjob" >/dev/null 2>&1; then
   (crontab -l 2>/dev/null; echo "$cronjob") | crontab -
 fi
